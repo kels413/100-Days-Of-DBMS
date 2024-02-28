38ae087761f4a5fb3d648231d6c28f55d3a54247
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-28 14:21:17 +0100

    concat strings

--- SQL/querry_book.sql
+++ SQL/querry_book.sql
@@ -70,9 +70,7 @@ SELECT
     CONCAT(
         SUBSTR(title, 1, 10),
         '...'
-    ) AS 'short title'
+    ) AS 'short title', 
+    CONCAT(author_lname, ',', author_fname) AS 'author',
+    CONCAT(stock_quantity, ' in stock') AS quantity
 FROM books;
-SELECT
- CONCAT(author_lname, ',', author_fname) AS 'author'
- FROM books
-
