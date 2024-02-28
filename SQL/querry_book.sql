097e9077cd5d38a96731be0a975c32116125883e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-28 13:35:39 +0100

    reverse and uppercase

--- SQL/querry_book.sql
+++ SQL/querry_book.sql
@@ -11,3 +11,8 @@ FROM books;
 -- REVERSE AND UPPERCASE A STATEMENT
 -- 'why does my cat look at me with such hatred'
 
+SELECT
+    REVERSE(
+        UPPER('why does my cat look at  me with such hatred')
+    );
+
