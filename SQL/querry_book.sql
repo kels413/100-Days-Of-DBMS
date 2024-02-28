b9592f2b5a6ae8df6cbf3bc70d8adbae522d6aeb
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-28 13:33:20 +0100

    reverse and uppercase

---
+++ SQL/querry_book.sql
@@ -0,0 +1,13 @@
+USE book_shop
+
+-- CONCAT A SUBSTRING
+SELECT 
+    CONCAT(
+        SUBSTRING(title, 1, 10),
+        "..."
+    ) AS 'short_title'
+FROM books;
+
+-- REVERSE AND UPPERCASE A STATEMENT
+-- 'why does my cat look at me with such hatred'
+
