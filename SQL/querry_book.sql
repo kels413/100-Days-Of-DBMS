854e5ec510c05c42365d62ad419bb193eea5eccb
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-28 14:17:35 +0100

    concat strings

--- SQL/querry_book.sql
+++ SQL/querry_book.sql
@@ -66,4 +66,13 @@ WHERE author_lname = 'Gaiman';
 
 -- concat alot of strings.
 
+SELECT 
+    CONCAT(
+        SUBSTR(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
+SELECT
+ CONCAT(author_lname, ',', author_fname) AS 'author'
+ FROM books
 
