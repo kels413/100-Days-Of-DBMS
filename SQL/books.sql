3bb35d4598d790180cdf62e8a739451c9100a9e5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-06 21:31:28 +0100

    updated books file

--- SQL/books.sql
+++ SQL/books.sql
@@ -34,7 +34,7 @@ VALUES
 
 
 
-INSERT INTO books
+eINSERT INTO books
     (title, author_fname, author_lname, released_year, stock_quantity, pages)
   VALUES ('10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
           ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
