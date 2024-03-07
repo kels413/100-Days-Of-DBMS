acb4063b4248078b06e224da00114dab0486f256
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-07 18:11:12 +0100

    deleted records

--- SQL/libray.sql
+++ SQL/libray.sql
@@ -2,10 +2,28 @@
 -- database name: customers_and_orders
 -- tables : customers table and orders table.g
 
-CREATE DATABASE `customers_and_orders`
+-- create DATABASE
+CREATE DATABASE IF NOT EXISTS `customers_and_orders`
 
--- 
+-- USE DB
+USE customers_and_orders
 
+--  CREATE TABLE
+
+CREATE TABLE IF NOT EXISTS `customers`(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        email VARCHAR(30)
+)
+
+CREATE TABLE IF NOT EXISTS `orders`(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        order_date DATE,
+        amount DECIMAL(8, 2),
+        customer_id INT,
+        FOREIGN KEY(customer_id) REFERENCES customers(id)
+)
 
 INSERT INTO customers (first_name, last_name, email) 
 VALUES ('Boy', 'George', 'george@gmail.com'),
@@ -20,3 +38,13 @@ VALUES ('2016/02/10', 99.99, 1),
        ('2014/12/12', 800.67, 2),
        ('2015/01/03', 12.50, 2),
        ('1999/04/11', 450.25, 5);
+
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016/02/10', 99.99, 1)
+
+SELECT * FROM customers
+
+SELECT * FROM orders
+
+
+
