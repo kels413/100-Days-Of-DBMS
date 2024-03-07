b8b2b128d4cc3bbc2a6c0529be7c96cfd16449f3
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-07 17:56:38 +0100

    insert into customers table

--- SQL/libray.sql
+++ SQL/libray.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE `customers_and_orders`
 
+
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
