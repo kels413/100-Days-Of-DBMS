a2dd72b4c6f73258913d2cff70a0d62213162dea
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-07 18:38:53 +0100

    subquery

--- SQL/libray.sql
+++ SQL/libray.sql
@@ -52,5 +52,15 @@ SELECT * FROM customers WHERE id = 1
 
 SELECT * FROM orders WHERE customer_id  = 1
 
+-- using subquery to join the select
+
+SELECT * FROM orders
+ WHERE customer_id = 
+                    (
+                        SELECT id FROM customers
+                        WHERE last_name = "George"
+                    );
+
+
 
 
