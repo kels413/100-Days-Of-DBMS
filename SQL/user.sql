38aa2406b7cee31ff00ae5d15df2b6574478fff4
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 14:37:39 +0100

    grants

--- SQL/user.sql
+++ SQL/user.sql
@@ -12,7 +12,15 @@ SELECT CURRENT_ROLE();
 
 SELECT CURRENT_USER()
 
-CREATE USER 'Kelly chukwu'@'localhost' IDENTIFIED BY '31006569'
+CREATE USER 'Kelly chukwu'@'localhost' IDENTIFIED BY '11111'
 
 SELECT USER, HOST FROM mysql.USER
+
+show PRIVILEGES;
+
+DROP USER 'kelly chukwu'@'localhost';
+
+CREATE user 'ayo'@'localhost' IDENTIFIED BY '11111'
+
+SELECT USER, HOST FROM mysql.user
 
