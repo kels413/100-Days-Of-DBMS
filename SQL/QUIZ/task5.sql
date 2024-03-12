f0517ad1dcca12ce02d3845f99d6f80c3373ce9c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 23:32:29 +0100

    sum sales amount

---
+++ SQL/QUIZ/task5.sql
@@ -0,0 +1,22 @@
+-- Aggregation with Inner Join:
+-- Write a query to calculate the total sales amount for each customer, considering only orders placed after January 1, 2023.
+
+
+-- sales amount (sum)
+-- each customer (GROUP BY)
+
+SELECT Customers.customer_name,
+       Customers.customer_id,
+       ORDERS.order_date,
+       SUM(ORDERS.order_amount) AS 'SUM'
+FROM Customers
+JOIN ORDERS
+ON  Customers.customer_id = ORDERS.customer_id
+WHERE ORDERS.order_date > '2023-03-15'
+GROUP BY Customers.customer_name, Customers.customer_id, ORDERS.order_date;
+
+
+
+
+
+
