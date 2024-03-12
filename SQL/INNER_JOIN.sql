4a9e3e1cd8d9a11b7310673d5460823aa85a6e7f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 21:13:44 +0100

    join

--- SQL/INNER_JOIN.sql
+++ SQL/INNER_JOIN.sql
@@ -4,57 +4,66 @@ USE `management`
 
 -- Create the Departments table
 CREATE TABLE Departments (
-    department_id INT PRIMARY KEY,
+    department_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
     department_name VARCHAR(50)
 );
 
 -- Insert some sample data into the Departments table
-INSERT INTO Departments (department_id, department_name) VALUES
-(1, 'Sales'),
-(2, 'Marketing'),
-(3, 'Finance');
+INSERT INTO Departments (department_name) VALUES
+('Sales'),
+('Marketing'),
+('Finance');
+
+
+
 
 -- Create the Employees table
-CREATE TABLE Employees (
-    employee_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    department_id INT,
+CREATE TABLE IF NOT EXISTS Employees (
+    employee_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES Departments(department_id)
 );
 
 -- Insert some sample data into the Employees table
-INSERT INTO Employees (employee_id, name, department_id) VALUES
-(1, 'John Doe', 1),
-(2, 'Jane Smith', 2),
-(3, 'David Brown', 1),
-(4, 'Emily Johnson', 3),
-(5, 'Michael Lee', 2);
+INSERT INTO Employees (name, department_id) VALUES
+('John Doe', 1),
+('Jane Smith', 2),
+('David Brown', 1),
+('Emily Johnson', 3),
+('Michael Lee', 2);
 
+
+
+-------------------------------------------------------------------------------------------------------
 -- Create the Customers table
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
+    customer_id INT PRIMARY KEY AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
     customer_name VARCHAR(50)
 );
 
 -- Insert some sample data into the Customers table
-INSERT INTO Customers (customer_id, customer_name) VALUES
-(1, 'ABC Company'),
-(2, 'XYZ Corporation'),
-(3, '123 Industries');
+INSERT INTO Customers (customer_name) VALUES
+('ABC Company'),
+('XYZ Corporation'),
+('123 Industries');
 
 -- Create the Orders table
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
+    order_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Insert some sample data into the Orders table
-INSERT INTO Orders (order_id, customer_id, order_date) VALUES
-(1, 1, '2023-01-15'),
-(2, 2, '2023-02-20'),
-(3, 1, '2023-03-10'),
-(4, 3, '2023-04-05');
+INSERT INTO Orders (customer_id, order_date) VALUES
+(1, '2023-01-15'),
+(2, '2023-02-20'),
+(1, '2023-03-10'),
+(3, '2023-04-05');
 
-SHOW TABLES
+
+SELECT * from customers;
+
+SELECT * FROM Orders;
