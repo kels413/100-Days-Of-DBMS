09879d21d4f8e296a75378f1373993f08b84d244
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 23:38:36 +0100

    join managers and employee table

---
+++ SQL/QUIZ/task6.sql
@@ -0,0 +1,7 @@
+--Self Join:
+--Suppose you have a table named "Employees" with columns (employee_id, name, manager_id). Write a query to
+--retrieve the names of all employees along with the names of their respective managers.
+
+SELECT Employees.name, managers.name FROM EMPloyees
+JOIN managers ON `EMPloyees`.employee_id = `managers`.manager_id;
+
