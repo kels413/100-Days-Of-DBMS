8e64b8634992f4beb434e96461b5e169afe1be10
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 00:02:53 +0100

    multiple table

---
+++ SQL/QUIZ/task7.sql
@@ -0,0 +1,24 @@
+-- Joining Multiple Tables:
+--Suppose you have tables named "Students", "Courses", and "Enrollments" with appropriate columns. Write a 
+--query to retrieve the names of all students along with the names of the courses they are enrolled in.
+
+
+-- Tables
+    -- Student, Courses, Enrollments.
+
+-- student table
+    -- student_id
+
+-- courses_table
+    -- student_id
+
+-- Enrollments_table
+    -- student_table
+
+-- querry
+    -- all student name.
+    -- students courses.
+
+
+
+
