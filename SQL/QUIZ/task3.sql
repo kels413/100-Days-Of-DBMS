9f64f16262b4b9041c8420b5796d23aa77758b0f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 22:10:43 +0100

    filter employee (sales table)

--- SQL/QUIZ/task3.sql
+++ SQL/QUIZ/task3.sql
@@ -1,3 +1,11 @@
 -- Filtering with Inner Join:
 -- Modify the previous query to only retrieve employees who work in the 'Sales' department.
 
+SELECT * from employees
+
+SELECT employees.name FROM employees
+JOIN Departments
+ON employees.employee_id = Departments.department_id
+WHERE Departments.department_name = 'sales'
+
+SELECT employees.name 
