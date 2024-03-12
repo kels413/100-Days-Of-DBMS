7053ae66c6f6d38b59d5bfb02c5e9beaa5378fc5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 22:56:50 +0100

    added task4

--- SQL/QUIZ/task4.sql
+++ SQL/QUIZ/task4.sql
@@ -1,18 +1,12 @@
 -- Multiple Table Join:
--- Create a query to list the names of all customers who have made orders, along with the order IDs and order dates.
-
-use management;
-
-SELECT * FROM customers;
-
-SELECT * FROM ORDERS;
-
+-- Create a query to list the names of all customers who have made orders, along with the order IDs and order dates
 
 -- names of all customers (customer table)
 -- placed order (order table)
 -- order_id (order table)
 -- order_dates (order_table)
 
-SELECT customer_name AS "name" FROM Customers
+
+SELECT customer_name AS 'name', order_date AS 'order_date', order_id AS 'order_id' FROM Customers
 JOIN ORDERS
 ON Customers.customer_id = ORDERS.customer_id
