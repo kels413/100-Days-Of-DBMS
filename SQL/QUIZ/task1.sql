af74f289d6d80465ae5f2069e5d3e348b7ef335a
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 23:39:56 +0100

    modified

--- SQL/QUIZ/task1.sql
+++ SQL/QUIZ/task1.sql
@@ -2,6 +2,13 @@
 -- Write a query to retrieve the names of all employees along with their department names.
 
 
-SELECT Employees.name, Departments.department_name From Employees
+SELECT Employees.name,Departments.department_name, COUNT(Departments.department_name) AS 'COUNT' From Employees
 JOIN Departments
-ON `Employees`.department_id = Departments.department_id;
+ON `Employees`.department_id = Departments.department_id
+WHERE `Departments`.department_name = 'sales' OR `Departments`.department_name = 'Marketing'
+GROUP BY Departments.department_name, Employees.name
+
+
+SELECT * FROM departments;
+
+SELECT * FROM Employees
