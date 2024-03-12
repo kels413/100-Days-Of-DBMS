6f463c1ecb7d51b64847d746470aa316512377d8
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 21:47:16 +0100

    basic join

--- SQL/QUIZ/task1.sql
+++ SQL/QUIZ/task1.sql
@@ -1,3 +1,7 @@
 -- Basic Inner Join:
 -- Write a query to retrieve the names of all employees along with their department names.
 
+
+SELECT Employees.name, Departments.department_name From Employees
+JOIN Departments
+ON `Employees`.department_id = Departments.department_id;
