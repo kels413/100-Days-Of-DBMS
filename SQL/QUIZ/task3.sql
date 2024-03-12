c913e749d469d0301e812a40cf729e7db54ef353
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-12 22:33:14 +0100

    filter employee (sales table) using subquerry

--- SQL/QUIZ/task3.sql
+++ SQL/QUIZ/task3.sql
@@ -10,5 +10,12 @@ WHERE Departments.department_name = 'sales'
 SELECT department_id, department_name from Departments WHERE `Departments`.department_id in
 (SELECT employee_id FROM employees);
 
+-- using subquerry
+
+SELECT name,
+(SELECT department_name FROM Departments WHERE `Departments`.department_id = employees.department_id)
+FROM 
+employees
+
 
 
