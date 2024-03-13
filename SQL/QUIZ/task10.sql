4b59edbf45f3abe7c31734979edd68aaa4c98cb9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 01:07:13 +0100

    difference btw where and on filter

---
+++ SQL/QUIZ/task10.sql
@@ -0,0 +1,7 @@
+-- Using WHERE vs. ON:
+--Explain the difference between applying a filter condition in the WHERE clause and in the ON clause when 
+--using inner joins.
+
+--Answer
+--WHERE clause: Filters are applied after the join, so it filters the final result set.
+--ON clause: Filters are applied during the join, affecting which rows are joined together.
